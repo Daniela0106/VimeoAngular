684e840dce2f3ba7a922b01c54111abe20c9a2ba
Daniela <daniela.barrientos.grisales@gmail.com>
2016-06-30 12:44:42 -0500

    The vid's id is at the bottom of the string, which is half of what's left to do. Just need to remove the part of the string jusqu'a l' nombre du id

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -3,6 +3,7 @@
 
   angular
     .module('vimeoAngular')
+    .controller('MainController', MainController)
     .config(routerConfig);
 
   /** @ngInject */
@@ -25,4 +26,71 @@
 
   }
 
+  /** @ngInject */
+  function MainController($http) {
+    var vm = this;
+
+    //--------------------- GET CATEGORIES' NAMES ----------------------------
+    $http({
+      method: 'GET',
+      url: 'https://api.vimeo.com/categories',
+      headers: {Authorization: 'Bearer effa86d0284056eacb391c194926a789'}
+    }).then(function successCallback(response) {
+      vm.responses = response;               //JSON response
+      var str = JSON.stringify(vm.responses);//Converting the JSON to String
+      var chunksOfResponse = str.split("/categories/");
+      var responseArray = [];
+      for (var i = 0; i < chunksOfResponse.length; i++) {
+        responseArray.push(chunksOfResponse[i]);
+        chunksOfResponse[i] += " ";
+      }
+      //vm.category_responses = responseArray; arreglo completo de las categorías
+      var categories = [];
+      var categoryName;
+      for (var j = 0; j < ((responseArray.length)); ++j) {
+        if (responseArray[j].indexOf("/channels") != -1) {
+          categoryName = responseArray[j].split("/channels").shift();
+          categories.push(categoryName);
+        }
+      }
+      vm.responses = categories;
+    }, function errorCallback(response){//Clled asynchly if error or srvr rtrns rspns w error status.
+      vm.responses = "There was an error while loading the Categories" + response;
+    });
+    //--------------------------------------------------------------------------
+
+    //-------------------------- VIDEOS' IMAGES --------------------------------
+    $http({
+      method: 'GET',
+      url: 'https://api.vimeo.com/categories/animation/videos?per_page=12',
+      headers: {Authorization: 'Bearer effa86d0284056eacb391c194926a789'}
+    }).then(function successCallback(responseVideos) {
+      vm.responseVideos = responseVideos; //JSON response
+      var str = JSON.stringify(vm.responseVideos);
+      var videoWord = str.split("iframe"); //*To have info split by video
+      var responseVideosArray = [];
+      for (var i = 0; i < videoWord.length; i++) {
+        responseVideosArray.push(videoWord[i]);
+        videoWord[i] += " ";
+      }
+
+      var video_images_id = [];
+      var video_image_id;
+      for (var k=0; k< responseVideosArray.length; ++k){
+        if(responseVideosArray[k].indexOf("\"link\":\"https://i.vimeocdn.com/video/") != -1){ //If this part exists in the string
+          video_image_id = responseVideosArray[k].split("_200x150").shift(); //*So that vid's id is @ the end
+          video_images_id.push(video_image_id);
+        }
+      }
+
+      //removing first part of the string
+
+
+
+      vm.response_videos = video_images_id;//<------ DEL
+    });
+    //src=https://i.vimeocdn.com/video
+    //--------------------------------------------------------------------------
+  }
 })();
+//https://i.vimeocdn
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,72 +3,11 @@
 
   angular
     .module('vimeoAngular')
-    .controller('MainController', MainController);
-
-  /** @ngInject */
-  function MainController($http) {
-    var vm = this;
-    //vm.search_history = ["Recipes", "Colombian music", "Sports", "Gospel"];
-
-    $http({
-      method: 'GET',
-      url: 'https://api.vimeo.com/categories',
-      headers: {Authorization: 'Bearer effa86d0284056eacb391c194926a789'}
-    }).then(function successCallback(response) {
-      vm.responses = response; //JSON response
-      var str = JSON.stringify(vm.responses);//strResponse;
-      var words = str.split("/categories/");
-      var responseArray = [];
-      for (var i = 0; i < words.length - 1; i++) {
-        responseArray.push(words[i]);
-        words[i] += " ";
-      }
-      vm.category_responses = responseArray;
-
-      //--------------------- GET CATEGORIES' NAMES ------------------------------
-      var categories = [];
-      var categoryName;
-      for (var j = 0; j < ((responseArray.length)); ++j) {
-        if (responseArray[j].indexOf("/channels") != -1) {
-          categoryName = responseArray[j].split("/channels").shift();
-          categories.push(categoryName);
-        } 
-      }
-      vm.responses = categories;
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-      vm.responses = "There was an error while loading the Categories" + response;
-    });
-    //------------------------------------------------------------------------------
-
-    //--------------------- VIDEOS' IMAGES -----------------------------------------
-    $http({
-      method: 'GET',
-      url: 'https://api.vimeo.com/categories/animation/videos?per_page=12',
-      headers: {Authorization: 'Bearer effa86d0284056eacb391c194926a789'}
-    }).then(function successCallback(responseVideos) {
-      vm.responseVideos = responseVideos;
-      var str = JSON.stringify(vm.responseVideos);
-      var videoWord = str.split("/categories/");
-      var responseVideosArray = [];
-      for (var i = 0; i < videoWord.length - 1; i++) {
-        responseVideosArray.push(videoWord[i]);
-        videoWord[i] += " ";
-      }
-      vm.response_videos = responseVideosArray;
-    });
-
-    //I NEED TO INVOKE THIS VARIABLE FROM THE DOM
-    vm.video_ejemplo = "https://player.vimeo.com/video/108650530?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0";
-    //per_page=12&offset=
-  }
-
-
+  
   //---------------------------- SHOW CATEGORIES -----------------------------------
   //On image ☰ click display category_container
   //On .closeX click hide category_container
 
   //--------------------------------------------------------------------------------
-
+  
 })();
